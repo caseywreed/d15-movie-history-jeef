0eb41fb5d9ea20be6ad9320c55b104c56b4fe584
Edgar Barajas <ebarajas05@gmail.com>
2016-08-29 11:24:00 -0500

    created working handlebars template

---
+++ js/hbcontrols.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let $ = require('jquery');
+
+var Handlebars = require("hbsfy/runtime");
+var movieTemplate = require('../templates/movie-list.hbs');
+var savedTemplate = require('../templates/final-card.hbs');
+// var searchTemplate = require('../templates/title.hbs');
+// var movieResultsArray = require('./main.js');
+
+// Register a partial
+// Handlebars.registerPartial("tagLine", require('../templates/partials/tagline.hbs'));
+
+// Create a custome helper that we can call inside an expression in
+// our markup
+// Handlebars.registerHelper("inc", function(value) {
+//   return parseInt(value) + 1;
+// });
+
+let displayAll = function(movieResultsArray) {
+    console.log("results", movieResultsArray);
+    let movieList = {movies: movieResultsArray};
+    $(".mhMain").html("");
+    $(".mhMain").html(savedTemplate(movieList));
+    // $("#ssPop").prop('disabled', true);
+  };
+
+module.exports = {displayAll};
+
