258848f9064b737057aefd80e67171e6b6dfc13f
Casey Reed <casey.w.reed@gmail.com>
2016-08-30 10:35:30 -0500

    images added

--- dist/app.js
+++ dist/app.js
@@ -11808,7 +11808,9 @@ var HandlebarsCompiler = require('hbsfy/runtime');
 module.exports = HandlebarsCompiler.template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "  <div class=\"col s4\">\n    <div class=\"card blue-grey darken-1 cardContainer\">\n      <div class=\"card-content white-text\">\n        <span data-delete-id=\""
+  return "  <div class=\"col s4\">\n    <div class=\"card blue-grey darken-1 cardContainer\">\n      <div class=\"card-content white-text\">\n        <img class=\"poster-image\" src=\""
+    + alias4(((helper = (helper = helpers.Poster || (depth0 != null ? depth0.Poster : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Poster","hash":{},"data":data}) : helper)))
+    + "\">\n        <span data-delete-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\" class=\"chip deleteChip\">Delete<i class=\"close material-icons\">close</i></span>\n        <span class=\"card-title\" id=\"movieTitle"
     + alias4(((helper = (helper = helpers.imdbID || (depth0 != null ? depth0.imdbID : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imdbID","hash":{},"data":data}) : helper)))
