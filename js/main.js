b8ddeca9b090e0db417719e99a5ef322da4e69e6
Dayne Wright <daynewr@gmail.com>
2016-08-30 10:22:16 -0500

    removed debugger

--- js/main.js
+++ js/main.js
@@ -52,7 +52,6 @@ $("a").click(function(e){
 $("#searchMovies").click(function() {
   let searchQuery = $("#movieTitleInput").val();
   console.log("clicked search");
-  debugger;
   db.searchMovies(searchQuery).then( function (movieTitles) {
     var movieTitlesArray = [];
     $.each(movieTitles.Search, function (index, key) {
