fae9c3b0b96a38f50f5872abd91832ce0b2519a2
Casey Reed <casey.w.reed@gmail.com>
2016-08-30 14:46:28 -0500

    Updated displayAll to be primary display function

--- js/hbcontrols.js
+++ js/hbcontrols.js
@@ -5,26 +5,18 @@ let $ = require('jquery');
 var Handlebars = require("hbsfy/runtime");
 var movieTemplate = require('../templates/movie-list.hbs');
 var savedTemplate = require('../templates/final-card.hbs');
-// var searchTemplate = require('../templates/title.hbs');
-// var movieResultsArray = require('./main.js');
 
-// Register a partial
-// Handlebars.registerPartial("tagLine", require('../templates/partials/tagline.hbs'));
-
-// Create a custome helper that we can call inside an expression in
-// our markup
-// Handlebars.registerHelper("inc", function(value) {
-//   return parseInt(value) + 1;
-// });
 
 let displayAll = function(movieResultsArray) {
     return new Promise(function(resolve, reject){
-      console.log("results", movieResultsArray);
+        // GROSS!
+      for (var movie in movieResultsArray.movies) {
+        movieResultsArray.movies[movie].id = movie
+      }
       let movieList = {movies: movieResultsArray};
       $(".mhMain").html("");
       $(".mhMain").append(savedTemplate(movieList));
-      // $("#ssPop").prop('disabled', true);
-      resolve();
+      resolve(movieResultsArray);
     })
   };
 
